94f8ff54f282e62c5fcfcada61b76a3c818ba5c1
jambun <jambun@gmail.com>
2017-05-22 14:11:08 +1000

    first running version

---
+++ backend/job_runners/aspace_check_runner.rb
@@ -0,0 +1,106 @@
+require 'jsonmodel'
+
+class ASpaceCheckRunner < JobRunner
+
+  register_for_job_type('aspace_check_job', :allow_reregister => true)
+#  register_for_job_type('aspace_check_job', :create_permissions => :manage_repository,
+#                                            :cancel_permissions => :manage_repository)
+
+  def run
+    # Some models are repo scoped but don't have a repo_id.
+    # At the time of writing, these:
+    #   CollectionManagement, Deaccession, RdeTemplate,
+    #   RevisionStatement, UserDefined
+    #
+    # This requires a little hoop jumping, like:
+    #   - giving a repo context to the global check
+    #   - checking if a model has a repo_id column
+    #
+    # This is annoying, but what are you going to do?
+
+    @total_count = 0
+    @invalid_count = 0
+    @error_count = 0
+
+    log("asck - ArchivesSpace Data Checker")
+    log("---------------------------------")
+
+    # globals first
+    log("Global:")
+    RequestContext.open(:repo_id => 0) do
+      check_models(:global => true)
+    end
+
+    # then by repo
+    Repository.each do |repo|
+      break if self.canceled?
+      log("--")
+      log("Repository: #{repo.repo_code} (id=#{repo.id})")
+      RequestContext.open(:repo_id => repo.id) do
+        check_models
+      end
+    end
+
+    log("--")
+    
+    if self.canceled?
+      log("Check canceled! Incomplete results follow.")
+    else
+      log("Check complete.")
+    end
+    log("#{@total_count} record#{@total_count == 1 ? '' : 's'} found in #{Repository.count} repositories.")
+    log("#{@invalid_count} record#{@invalid_count == 1 ? '' : 's'} are invalid.")
+    log("#{@error_count} record#{@error_count == 1 ? '' : 's'} errored.")
+    self.success! unless self.canceled?
+  end
+
+
+  def check_models(opts = {})
+    global = opts.fetch(:global, false)
+
+    ASModel.all_models.each do |model|
+      break if self.canceled?
+
+      next unless model.has_jsonmodel?
+      next unless model.model_scope(true)
+
+      # some models declare themselves as repo scoped, but don't have repo_ids
+      # treat them as globals
+      next if (model.model_scope == :global || !model.columns.include?(:repo_id)) != global
+
+      if global
+        check_records(model)
+      else
+        check_records(model, model.where(:repo_id => model.active_repository))
+      end
+    end
+  end
+
+
+  def check_records(model, ds = nil)
+    ds ||= model
+
+    @total_count += ds.count
+    log("#{model}: #{ds.count}")
+
+    ds.each do |record|
+      break if self.canceled?
+      begin
+        json = model.to_jsonmodel(record[:id])
+      rescue JSONModel::ValidationException => e
+        @invalid_count += 1
+        log("  * Invalid record: #{model} #{record[:id]} -- #{e}")
+      rescue => e
+        @error_count += 1
+        log("  * Record errored: #{model} #{record[:id]} -- #{e}")
+      end
+    end
+  end
+
+
+  def log(s)
+    Log.debug(s)
+    @job.write_output(s)
+  end
+
+end
